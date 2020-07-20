e32751f609cde4ac7d8a5d2d48a85e9a16e5bd19
priya mohata <mohatapriya@gmail.com>
2020-07-20 20:20:36 +0530

    third commit add random function

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ var colors=["rgb(117, 77, 176)",
             "rgb(208, 230, 122)",
             "rgb(77, 176, 92)"];
 var squares=document.querySelectorAll(".square");
-var pickedColor=colors[3];
-var picked=document.querySelector("h1");
-picked.innerHTML="The Great<br>"+pickedColor+"<br>Color Game";
+var pickedColor=pickColor();
+var picked=document.querySelector("h1 span");
+picked.innerHTML=pickedColor;
+var picked1=document.querySelector("h1");
+var messageDisplay=document.querySelector("#textDisplay");
 for(var i=0;i<squares.length;i++)
 {
     //adding initial colors
@@ -19,12 +21,25 @@ for(var i=0;i<squares.length;i++)
         //This is used to compare
         if(comparedColor===pickedColor)
         {
-        alert("yes right!");
+        // alert("yes right!");
+        for(var i=0;i<squares.length;i++){
+        squares[i].style.backgroundColor=pickedColor;
+        }
+        picked1.style.backgroundColor=pickedColor;
+        messageDisplay.textContent="Correct";
         }
         else
         {
-               alert("wrong");
+            this.style.backgroundColor="black"
+            //    alert("wrong");
+            messageDisplay.textContent="Try Again";
         }
     });
+}
+function pickColor(){
+    //In order to generate the random index in the array
+    var index=Math.floor(Math.random()*colors.length);
+    return(colors[index]);
+
 }
 
